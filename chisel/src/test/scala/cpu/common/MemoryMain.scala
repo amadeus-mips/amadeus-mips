a04ae255275d045a2a13506450a13704d5d71f1b
Tianrui Wei <tianrui-wei@outlook.com>
2020-02-01 22:12:29 +0800

    added a comment on how to run the tests

--- chisel/src/test/scala/cpu/common/MemoryMain.scala
+++ chisel/src/test/scala/cpu/common/MemoryMain.scala
@@ -1,6 +1,7 @@
 package cpu.common
 import chisel3._
 object MemoryMain extends App {
+  // run test:runMain cpu.common.MemoryMain in sbt
   iotesters.Driver.execute(args, () => new InstrMem(32,32,1024)) {
     c => new MemoryUnitTester(c)
   }
