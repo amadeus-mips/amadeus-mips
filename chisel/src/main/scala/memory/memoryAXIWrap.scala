df1bd4ba1bcdd168727a896327ca690c64f6fbe8
discreater <discreater@163.com>
2020-04-09 17:45:30 +0800

    feat: add soc test(from loognson)
    - remove memory address limit

---
+++ chisel/src/main/scala/memory/memoryAXIWrap.scala
@@ -0,0 +1,22 @@
+// See README.md for license details.
+package memory
+
+import chisel3._
+import common.AXIIO
+import memory.axi.AXI1x2SramInterface
+import memory.physicalMem.{DMemCombinationalPortForAXI, DualPortedCombinMemory, IMemCombinationalPortForAXI}
+
+class memoryAXIWrap(memFile: String) extends Module {
+  val io = IO(new Bundle {
+    val axi = AXIIO.slave()
+  })
+  val mem = Module(new DualPortedCombinMemory(1 << 20, memFile))
+  val imem = Module(new IMemCombinationalPortForAXI)
+  val dmem = Module(new DMemCombinationalPortForAXI)
+  mem.wireMemAXIPort(imem, dmem)
+  val axiInterface = Module(new AXI1x2SramInterface)
+
+  axiInterface.io.dram <> dmem.io.axi
+  axiInterface.io.iram <> imem.io.axi
+  axiInterface.io.bus <> io.axi
+}
