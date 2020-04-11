bea948c6e48e32695536fd44f6ca148aba5aa8de
Tianrui Wei <tianrui-wei@outlook.com>
2020-04-11 10:05:52 +0800

    small fix in the syntax of the script

--- chisel/utils/assemble.bash
+++ chisel/utils/assemble.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar Mars4_5.jar mc CompactTextAtZero a dump .text HexText ../testMemFile/$1/$2.txt ../testMemFile/$1/$2.asm
+java -jar Mars4_5.jar mc CompactTextAtZero a dump .text HexText ../testMemFile/"$1"/"$2".txt ../testMemFile/"$1"/"$2".asm
--- chisel/utils/assembleAll.bash
+++ chisel/utils/assembleAll.bash
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-for i in `find ../testMemFile/$1 -type f -name "*.asm"` ; do
+# shellcheck disable=SC2044
+for i in $(find ../testMemFile/"$1" -type f -name "*.asm") ; do
   FULLNAME=$i
   k=${FULLNAME%.*}
-  echo $k
+  echo "$k"
   java -jar ~/Desktop/Mars4_5.jar mc CompactTextAtZero a dump .text HexText ${k}.txt  $i
 done
