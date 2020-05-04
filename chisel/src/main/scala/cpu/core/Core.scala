3b10a2bad199bd4eb92f199ae25be559fed8373c
Discreater <discreater@163.com>
2020-05-05 01:15:08 +0800

    fix: won't flush the if_id, when branch predict error with the id_exe stalled.
    - When id_exe stalled, the delay slot instruction can't move to the execute stage, and once if_id flushed, this instruction will disappeared.

--- chisel/src/main/scala/cpu/core/Core.scala
+++ chisel/src/main/scala/cpu/core/Core.scala
@@ -48,7 +48,7 @@ class Core extends MultiIOModule {
 
   if_id.io.in    := fetchTop.io.out
   if_id.io.stall := hazard.io.stall
-  if_id.io.flush := hazard.io.flush || (!fetchTop.io.out.inDelaySlot && executeTop.io.branch.valid)
+  if_id.io.flush := hazard.io.flush || (!fetchTop.io.out.inDelaySlot && executeTop.io.branch.valid && !hazard.io.stall(3))
 
   val inst = Buffer(in = io.rInst.data, en = !hazard.io.flush && hazard.io.stall(0)).io.out
 
