a86ab1bfe4e372567190cd671f2bd4eb3ba1c8a1
Discreater <discreater@163.com>
2020-06-25 16:36:25 +0800

    chore: move multiply to the second cycle

--- chisel/src/main/scala/cpu/core/components/Mult.scala
+++ chisel/src/main/scala/cpu/core/components/Mult.scala
@@ -12,7 +12,9 @@ class Mult(implicit conf: CPUConfig) extends Module {
   val io = IO(new MultDivIO)
   io.result.hi.valid := RegNext(io.enable, false.B)
   io.result.lo.valid := RegNext(io.enable, false.B)
-  val result = RegNext(Mux(io.signed, (io.op1.asSInt() * io.op2.asSInt()).asUInt(), io.op1 * io.op2), 0.U(32.W))
+  val op1 = RegNext(io.op1, 0.U(32.W))
+  val op2 = RegNext(io.op2, 0.U(32.W))
+  val result = Mux(io.signed, (op1.asSInt() * op2.asSInt()).asUInt(), op1 * op2)
   io.result.hi.bits := result(63, 32)
   io.result.lo.bits := result(31, 0)
 }
