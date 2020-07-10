eafdcd03a0bd1df3143a30731b32e5a72de87a27
Tianrui Wei <tianrui-wei@outlook.com>
2020-07-10 10:47:03 +0800

    seperated data path from control path for instruction cache

--- chisel/src/main/scala/cpu/pipelinedCache/instCache/ICacheController.scala
+++ chisel/src/main/scala/cpu/pipelinedCache/instCache/ICacheController.scala
@@ -2,38 +2,32 @@ package cpu.pipelinedCache.instCache
 
 import chisel3._
 import chisel3.internal.naming.chiselName
-import chisel3.util._
 import cpu.pipelinedCache.CacheConfig
 
-//TODO: add flush support
 @chiselName
 class ICacheController(implicit cacheConfig: CacheConfig) extends Module {
   val io = IO(new Bundle {
-    val isMiss = Input(Bool())
-    val finishTransfer = Input(Bool())
-    val stall = Output(Bool())
-    val writeBack = Output(Bool())
+
+    /** flush request */
+    val flushReq = Input(Bool())
+
+    /** is stage 2 free */
+    val stage2Free = Input(Bool())
+
+    /** is this cycle a writeback */
+    val writeBack = Input(Bool())
+
+    /** ready for next instruction */
+    val reqReady = Output(Bool())
+
+    /** write enable for instruction banks, for fetch stage */
+    val writeEnable = Output(Bool())
+
+    /** flush stage 2 registers */
+    val flush = Output(Bool())
   })
 
-  val sIdle :: sMiss :: sWriteBack :: Nil = Enum(3)
-  val state = RegInit(sIdle)
-
-  io.stall := state =/= sIdle && io.isMiss
-  io.writeBack := state === sWriteBack
-
-  switch(state) {
-    is(sIdle) {
-      when(io.isMiss) {
-        state := sMiss
-      }
-    }
-    is(sMiss) {
-      when(io.finishTransfer) {
-        state := sWriteBack
-      }
-    }
-    is(sWriteBack) {
-      state := sIdle
-    }
-  }
+  io.reqReady    := io.stage2Free && !io.writeBack
+  io.flush       := io.flushReq
+  io.writeEnable := io.writeBack
 }
---
+++ chisel/src/main/scala/cpu/pipelinedCache/instCache/QueryTop.scala
@@ -0,0 +1,125 @@
+package cpu.pipelinedCache.instCache
+
+import axi.AXIIO
+import chisel3._
+import chisel3.internal.naming.chiselName
+import chisel3.util._
+import cpu.pipelinedCache.CacheConfig
+import cpu.pipelinedCache.components.{AXIReadPort, MissComparator, ReFillBuffer, ReadHolder}
+import cpu.pipelinedCache.instCache.fetch.FetchQueryBundle
+import shared.Constants.INST_ID
+import shared.LRU.PLRUMRUNM
+
+@chiselName
+class QueryTop(implicit cacheConfig: CacheConfig) extends Module {
+  val io = IO(new Bundle {
+    // control path IO
+    val flush = Input(Bool())
+    val ready = Output(Bool())
+    // datapath IO
+    val fetchQuery = Input(new FetchQueryBundle)
+    val bankData   = Input(Vec(cacheConfig.numOfWays, UInt((cacheConfig.bankWidth * 8).W)))
+    val data       = Decoupled(UInt(32.W))
+    // also data path, just write to instruction banks
+    val writeBundle = Output(new Bundle {
+      val writeEnable = Bool()
+      val writeWay    = UInt(log2Ceil(cacheConfig.numOfWays).W)
+      val writeIndex  = UInt(log2Ceil(cacheConfig.numOfSets).W)
+      val writeTag    = UInt(cacheConfig.tagLen.W)
+      val writeData   = Vec(cacheConfig.numOfBanks, UInt((cacheConfig.bankWidth * 8).W))
+    })
+    // axi port wiring
+    val axi = AXIIO.master()
+  })
+  // declare all the modules
+  val mshr         = Module(new MSHR)
+  val comparator   = Module(new MissComparator)
+  val axi          = Module(new AXIReadPort(addrReqWidth = 32, AXIID = INST_ID, burstLen = 16))
+  val refillBuffer = Module(new ReFillBuffer(false))
+  val lru          = PLRUMRUNM(numOfSets = cacheConfig.numOfSets, numOfWay = cacheConfig.numOfWays)
+  val readHolder   = Module(new ReadHolder)
+
+  /** do nothing to this query, proceed to next */
+  val passThrough = WireDefault(!io.fetchQuery.valid || io.flush)
+
+  /** is the query a hit in the bank */
+  val hitInBank = WireDefault(comparator.io.bankHitWay.valid)
+
+  /** is the query hit in the refill buffer */
+  val hitInRefillBuffer = WireDefault(comparator.io.hitInRefillBuffer)
+
+  /** is a new miss generated and *accepted* */
+  val newMiss = WireDefault(mshr.io.missAddr.fire)
+
+  /** is the query a hit in either places */
+  val queryHit = WireDefault(hitInBank || hitInRefillBuffer)
+
+  /** is the data.valid output high? */
+  val validData = WireDefault(queryHit && !passThrough)
+
+  /** io parts */
+
+  io.ready      := io.data.fire || passThrough
+  io.data.valid := validData
+  io.data.bits := MuxCase(
+    io.bankData(comparator.io.bankHitWay.bits),
+    Seq(
+      readHolder.io.output.valid      -> readHolder.io.output.bits,
+      comparator.io.hitInRefillBuffer -> refillBuffer.io.queryResult.bits
+    )
+  )
+
+  io.writeBundle.writeEnable := mshr.io.writeBack
+  io.writeBundle.writeWay    := lru.getLRU(mshr.io.mshrInfo.index)
+  io.writeBundle.writeIndex  := mshr.io.mshrInfo.index
+  io.writeBundle.writeTag    := mshr.io.mshrInfo.tag
+  io.writeBundle.writeData   := refillBuffer.io.allData
+
+  io.axi := DontCare
+  io.axi <> axi.io.axi
+
+  comparator.io.tagValid          := io.fetchQuery.tagValid
+  comparator.io.phyTag            := io.fetchQuery.phyTag
+  comparator.io.index             := io.fetchQuery.index
+  comparator.io.mshr.bits         := mshr.io.mshrInfo
+  comparator.io.mshr.valid        := !mshr.io.missAddr.ready
+  comparator.io.refillBufferValid := refillBuffer.io.queryResult.valid
+
+  refillBuffer.io.addr.valid := newMiss
+  refillBuffer.io.addr.bits  := io.fetchQuery.bankIndex
+  refillBuffer.io.inputData  := axi.io.transferData
+  refillBuffer.io.finish     := axi.io.finishTransfer
+
+  readHolder.io.input.valid := validData && !io.data.ready
+  readHolder.io.input.bits := MuxCase(
+    readHolder.io.output.bits,
+    Seq(
+      comparator.io.hitInRefillBuffer                                 -> refillBuffer.io.queryResult.bits,
+      (comparator.io.bankHitWay.valid && !readHolder.io.output.valid) -> io.bankData(comparator.io.bankHitWay.bits)
+    )
+  )
+
+  axi.io.addrReq.bits := Mux(
+    newMiss,
+    Cat(
+      io.fetchQuery.phyTag,
+      io.fetchQuery.index,
+      io.fetchQuery.bankIndex,
+      0.U(cacheConfig.bankOffsetLen.W)
+    ),
+    Cat(mshr.io.mshrInfo.asUInt, 0.U(cacheConfig.bankOffsetLen.W))
+  )
+  axi.io.addrReq.valid := newMiss
+
+  /** if there is a legitimate miss, i.e., valid request, didn't hit, and is not flushed */
+  mshr.io.missAddr.valid          := !queryHit && !passThrough
+  mshr.io.missAddr.bits.tag       := io.fetchQuery.phyTag
+  mshr.io.missAddr.bits.index     := io.fetchQuery.index
+  mshr.io.missAddr.bits.bankIndex := io.fetchQuery.bankIndex
+  mshr.io.readyForWB              := axi.io.finishTransfer
+
+  // update the LRU when there is a hit in the banks, don't update otherwise
+  when(hitInBank) {
+    lru.update(index = io.fetchQuery.index, way = comparator.io.bankHitWay.bits)
+  }
+}
