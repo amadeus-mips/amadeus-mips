e48f2fbe834de3d390523515097a336bcf86cbc6
Tianrui Wei <tianrui-wei@outlook.com>
2020-07-22 15:55:36 +0800

    now the dirty is in the party:)

--- chisel/src/main/scala/cpu/pipelinedCache/dataCache/query/QueryTop.scala
+++ chisel/src/main/scala/cpu/pipelinedCache/dataCache/query/QueryTop.scala
@@ -45,7 +45,7 @@ class QueryTop(implicit cacheConfig: CacheConfig) extends Module {
   val writeQueue = Module(new WriteQueue)
 
   /** keep all the dirty information, dirty(way)(index) */
-  val dirtyBanks = RegInit(VecInit(Seq.fill(cacheConfig.numOfWays)(VecInit(Seq.fill(cacheConfig.numOfSets)(false.B)))))
+  val dirtyBanks = RegInit(VecInit(Seq.fill(cacheConfig.numOfWays)(VecInit(Seq.fill(cacheConfig.numOfSets)(true.B)))))
 
   /** do nothing to this query, proceed to next */
   val passThrough = WireDefault(!io.fetchQuery.valid)
