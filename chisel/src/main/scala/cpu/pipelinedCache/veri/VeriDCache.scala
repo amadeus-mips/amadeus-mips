8dc093de0aec94f03427398447c6252f29710cc0
Tianrui Wei <tianrui-wei@outlook.com>
2020-08-01 14:24:57 +0800

    fixed a few hiccups in symbi yosys tool

--- chisel/src/main/scala/cpu/pipelinedCache/veri/VeriDCache.scala
+++ chisel/src/main/scala/cpu/pipelinedCache/veri/VeriDCache.scala
@@ -5,7 +5,7 @@ import chisel3.stage.{ChiselGeneratorAnnotation, ChiselStage}
 import chisel3.util._
 import cpu.CPUConfig
 import cpu.common.MemReqBundle
-import cpu.pipelinedCache.{CacheConfig, DataCache}
+import cpu.pipelinedCache.DataCache
 import firrtl.options.TargetDirAnnotation
 import verification.SymbiyosysAXIRam
 
@@ -13,10 +13,12 @@ class VeriDCache extends Module {
   val io = IO(new Bundle {
     val request = Flipped(Decoupled(new MemReqBundle))
     val data = Valid(UInt(32.W))
+    val cacheInstruction = Flipped(Decoupled(UInt(0.W)))
   })
   implicit val cpuConfig:   CPUConfig   = new CPUConfig(build = false, verification = true)
   val dcache  = Module(new DataCache(cpuConfig.dCacheConf))
   val veriRam = Module(new SymbiyosysAXIRam)
+  dcache.io.cacheInstruction <> io.cacheInstruction
   dcache.io.axi     <> veriRam.io.axi
   dcache.io.request <> io.request
   io.data.valid     := dcache.io.commit
