e42775b0fd3c94c2b5185092957c611b062a4a07
Discreater <discreater@163.com>
2020-08-04 17:32:12 +0800

    fix: mem won't request after handshake

--- chisel/src/main/scala/cpu/core/components/Stage.scala
+++ chisel/src/main/scala/cpu/core/components/Stage.scala
@@ -6,10 +6,11 @@ import chisel3._
 import cpu.core.Constants.cpuStallLen
 
 class StageIO[+T <: Data](gen: T) extends Bundle {
-  val stall = Input(UInt(cpuStallLen.W))
-  val flush = Input(Bool())
-  val in    = Input(gen)
-  val out   = Output(gen)
+  val stall   = Input(UInt(cpuStallLen.W))
+  val flush   = Input(Bool())
+  val in      = Input(gen)
+  val out     = Output(gen)
+  val stalled = Output(Bool())
 
   override def cloneType: this.type = new StageIO(gen).asInstanceOf[this.type]
 }
@@ -28,18 +29,23 @@ class Stage[+T <: Data](stageNumber: Int, gen: T) extends MultiIOModule {
   val io = IO(new StageIO[T](gen))
 
   val pipeReg = RegInit(0.U.asTypeOf(gen))
+  val stalled = RegInit(false.B)
 
   def stallEnd:   Bool = io.stall(stageNumber) && !io.stall(stageNumber + 1)
   def notStalled: Bool = !io.stall(stageNumber)
 
   when(io.flush || stallEnd) {
     pipeReg := 0.U.asTypeOf(gen)
+    stalled := false.B
   }.elsewhen(notStalled) {
       pipeReg := io.in
+      stalled := false.B
     }
     .otherwise {
       // stalled, do nothing
+      stalled := true.B
     }
-  io.out := pipeReg
+  io.out     := pipeReg
+  io.stalled := stalled
 
 }
