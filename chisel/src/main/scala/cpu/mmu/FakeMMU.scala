5395ac2db1db5478a61f0d2fd78efd7738c8bdbc
Discreater <discreater@163.com>
2020-08-08 21:55:51 +0800

    chore: remove unused file

--- chisel/src/main/scala/cpu/mmu/FakeMMU.scala
+++ chisel/src/main/scala/cpu/mmu/FakeMMU.scala
@@ -10,6 +10,6 @@ class FakeMMU(implicit conf: CPUConfig) extends BaseMMU {
   io.out.rInst.addr.bits := Cat(0.U(3.W), io.in.rInst.addr.bits(28, 0))
   io.out.memReq.tag      := Cat(0.U(3.W), io.in.memReq.tag(16, 0))
 
-  io.core := DontCare
-  io.core.except         := 0.U.asTypeOf(io.core.except)
+  io.core        := DontCare
+  io.core.except := 0.U.asTypeOf(io.core.except)
 }
